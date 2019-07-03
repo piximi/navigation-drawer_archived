d832b79e3355013b7ae35495696b96a89f6d78a1
Allen Goodman <allen.goodman@icloud.com>
2019-07-03 15:13:48 -0400

    FitClassifierDialog.stories

--- src/FitClassifierDialog/FitClassifierDialog/FitClassifierDialog.stories.tsx
+++ src/FitClassifierDialog/FitClassifierDialog/FitClassifierDialog.stories.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { FitClassifierDialog } from './FitClassifierDialog';
+import { Provider } from 'react-redux';
+import { store } from '@piximi/store';
+import { ThemeProvider } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core';
 
 const closeDialog = () => {};
 
+const theme = createMuiTheme({
+  palette: {
+    type: 'light'
+  }
+});
+
 storiesOf('FitClassifierDialog', module).add('example', () => (
-  <FitClassifierDialog
-    categories={[]}
-    closeDialog={closeDialog}
-    images={[]}
-    openedDialog={true}
-    openedDrawer={true}
-  />
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <FitClassifierDialog
+        categories={[]}
+        closeDialog={closeDialog}
+        images={[]}
+        openedDialog
+        openedDrawer={false}
+      />
+    </ThemeProvider>
+  </Provider>
 ));
