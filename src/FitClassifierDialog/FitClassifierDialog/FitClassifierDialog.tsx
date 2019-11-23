b67b81cb8a312819c6da71b32e60a3a0770fb980
Levin Moser <levin.moser@outlook.com>
2019-11-22 19:18:15 -0500

    v1.0.16

--- src/FitClassifierDialog/FitClassifierDialog/FitClassifierDialog.tsx
+++ src/FitClassifierDialog/FitClassifierDialog/FitClassifierDialog.tsx
@@ -1024,54 +1024,6 @@ export const FitClassifierDialog = (props: FitClassifierDialogProps) => {
               optimizationAlgorithm={optimizationAlgorithm}
             />
           </Collapse>
-
-          <ListItem
-            button
-            onClick={onDatasetSettingsListClick}
-            style={{ padding: '12px 0px' }}
-          >
-            <ListItemIcon>
-              {collapsedDatasetSettingsList ? (
-                <ExpandLessIcon />
-              ) : (
-                <ExpandMoreIcon />
-              )}
-            </ListItemIcon>
-
-            <ListItemText
-              primary="Dataset Settings"
-              style={{ fontSize: '1em' }}
-            />
-          </ListItem>
-
-          <Collapse
-            in={collapsedDatasetSettingsList}
-            timeout="auto"
-            unmountOnExit
-          >
-            <Tooltip title="Initialize dataset" placement="bottom">
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={initializeDatasets}
-              >
-                Initialize Dataset
-              </Button>
-            </Tooltip>
-
-            <div style={{ padding: '12px 0px', width: '300' }}>
-              <Typography id="range-slider" gutterBottom>
-                Dataset Splits
-              </Typography>
-              <Slider
-                value={datasetSplits}
-                onChange={handleChange}
-                valueLabelDisplay="auto"
-                aria-labelledby="range-slider"
-                getAriaValueText={valuetext}
-              />
-            </div>
-          </Collapse>
         </List>
       </DialogContent>
     </Dialog>
